0428d3dc2ffb06ed316458dcf103370db31ac998
Olivier <olivier.beguinti@gmail.com>
2020-01-14 14:07:51 -0500

    Step 2 of graph experimentation

--- Assets/Scripts/GraphBuilding/Graph.cs
+++ Assets/Scripts/GraphBuilding/Graph.cs
@@ -8,13 +8,24 @@ public class Graph : MonoBehaviour
 
     public Transform pointPrefab;
 
+    [Range(10, 100)]
+    public int resolution = 10;
+
     private void Awake()
     {
-        for (int i = -10; i < 10; i++)
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position;
+        position.z = 0f;
+
+        for (int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * i / 5f;
-            point.localScale = Vector3.one / 5f;
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
         }
     }
 
