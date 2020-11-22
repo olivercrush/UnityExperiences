701995e72e731d1869efe3879a0e5734a2d01090
olivercrush <olivier.beguinti@gmail.com>
2020-11-22 01:35:22 -0500

    Added new MapGenerator scene with random values

---
+++ Assets/MapGenerator/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int _width = 5;
+    public int _height = 5;
+    public GameObject _prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < _height; y++) {
+            for (int x = 0; x < _width; x++) {
+                GameObject cell = Instantiate(_prefab, new Vector3(x, 0, y), Quaternion.identity, transform);
+                cell.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+            }
+        }
+    }
+}
